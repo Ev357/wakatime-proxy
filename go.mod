a4ba42824216c5854283915f183c7fd7951330ea
Ev357 <evest.dev@gmail.com>
2024-08-21 23:03:55 +0200

    feat: env config

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.23.0
 
 require (
 	github.com/elazarl/goproxy v0.0.0-20240726154733-8b0c20506380 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	golang.org/x/net v0.26.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,24 +1,69 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/elazarl/goproxy"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+ 	godotenv.Load()
+
 	proxy := goproxy.NewProxyHttpServer()
 
-	proxy.OnRequest().DoFunc(func(req *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
-		req.Header.Add("X-Custom-Header", "Value")
+	proxy.OnRequest().DoFunc(func (req *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+			var headers map[string]string
 
-		return req, nil
-	})
+			headersEnv := os.Getenv("HEADERS")
 
-	log.Println("Proxy server is running on http://localhost:3000")
-	err := http.ListenAndServe(":3000", proxy)
+			err := json.Unmarshal([]byte(headersEnv), &headers)
+
+			if err != nil {
+				log.Printf("Error unmarshalling JSON: %v", err)
+				return req, nil
+			}
+
+			for key, value := range headers {
+				req.Header.Set(key, value)
+			}
+
+			return req, nil
+		})
+
+	proxy.OnRequest().HandleConnect(goproxy.AlwaysMitm)
+
+	portEnv := os.Getenv("PORT")
+	if portEnv == "" {
+		portEnv = "3000"
+	}
+	port, err := strconv.Atoi(portEnv)
+
 	if err != nil {
-		log.Fatalf("Failed to start server: %v", err)
+		log.Fatalf("Invalid port number: %s\n", portEnv)
+	}
+
+	verboseEnv := os.Getenv("VERBOSE")
+	if verboseEnv == "" {
+		verboseEnv = "f"
+	}
+	verbose, err := strconv.ParseBool(verboseEnv)
+
+	if err != nil {
+		log.Fatalf("Invalid verbose value: %s\n", verboseEnv)
+	}
+
+	proxy.Verbose = verbose
+
+	log.Printf("Proxy server is running on http://localhost:%d", port)
+	listenErr := http.ListenAndServe(fmt.Sprintf(":%d", port), proxy)
+
+	if listenErr != nil {
+		log.Fatalf("Failed to start server: %v", listenErr)
 	}
 }
